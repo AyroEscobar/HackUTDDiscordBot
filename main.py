6af962782592da66586a96874d4e76eeb08c91d1
Ayro Escobar <ayro.escobar@gmail.com>
2025-09-08 14:11:59 -0500

    Adding intents for bot

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import logging
 from dotenv import load_dotenv
 import os
 
+#Documentation Link: https://discordpy.readthedocs.io/en/stable/
+#source venv/bin/activate   to activate virtual environment
+
 load_dotenv()
 
+token = os.getenv('DISCORD_TOKEN')
+
+#parameters filename, encoding, mode (write)
+handler = logging.FileHandler('discord.log', 'utf-8', 'w')
+intents = discord.Intents.default()
+#If an intent is not working you might need to manually enable it
+intents.message_content = True
+intents.members = True
 
 
 
-#source venv/bin/activate   to activate virtual environment
+
+
 
