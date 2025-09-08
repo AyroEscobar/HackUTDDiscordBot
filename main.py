9aef588f97cc7c05aa839419969e07ecb8158c9b
Ayro Escobar <ayro.escobar@gmail.com>
2025-09-08 15:44:19 -0500

    Adding profanity checker for bot

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+from better_profanity import profanity
+
 
 #Documentation Link: https://discordpy.readthedocs.io/en/stable/
 #source venv/bin/activate   to activate virtual environment
@@ -14,6 +16,7 @@ Things required of the bot
 #Having commands to go back through information (displaying workshop schedule, locations of mentors, previous winners)
 ? Add verification of hackers through the bot (that they are not bots, so no mass sending a bunch of messages)
 Add Roles Mentors, Hackers, Sponsors, Organizers, MLH Coach, 
+Add a command for a ping pong game little thing
 
 
 '''
@@ -36,6 +39,29 @@ bot = commands.Bot(command_prefix ='!', intents = intents)
 @bot.event
 async def on_ready():
     print(f"We are ready to go in, {bot.user.name}")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server, {member.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if profanity.contains_profanity(message.content):
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - don't use that word!")
+
+    #Need to call this so that the overriding functions can keep being called
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+    
+
+    
 
 #Parameters token for discord, log_handler, log_level
 bot.run(token, log_handler = handler, log_level = logging.DEBUG)
